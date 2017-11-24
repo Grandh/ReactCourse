54997fe6b58666285c0b1a928ae44432b4a8802d
“Grandh” <296493452@qq.com>
2017-11-24 21:59:40 +0800

    2017_11_24 runoob study

---
+++ RunoobNode/ExpressTest/app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+app.get('/',function(req,res){
+    res.end("Hello world");
+});
+
+var server = app.listen(8888,function(){
+
+    var host = server.address().address;
+    var port = server.address().port;
+
+    console.log("http://%s:%s",host,port);
+})
+
